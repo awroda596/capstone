763af6e9cf45156d369560ddf329965d0ea07789
awroda596 <157451165+awroda596@users.noreply.github.com>
2025-05-15 12:15:24 -0700

    slight tweak for mobile view

--- frontend/lib/pages/user/dashboard.dart
+++ frontend/lib/pages/user/dashboard.dart
@@ -1,12 +1,14 @@
-//my attempt to modularize these functions.  Time perimitting will et them  modularized by before presentation but function first tbh.  
+//my attempt to modularize these functions.  Time perimitting will et them  modularized by before presentation but function first tbh.
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import '../../services/user.dart';
-import 'userInfo.dart'; 
+import 'userInfo.dart';
 import 'reviewList.dart';
 import 'sessionList.dart';
 import 'teaCabinet.dart';
+import 'dart:io';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
@@ -36,18 +38,34 @@ class _DashboardState extends State<Dashboard> {
           children: [
             UserInfo(),
             const SizedBox(height: 24),
-            Expanded(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Expanded(child: ReviewList()),
-                  SizedBox(width: 16),
-                  Expanded(child: TeaLogList()),
-                  SizedBox(width: 16),
-                  Expanded(child: TeaCabinetList()),
-                ],
-              ),
-            ),
+            kIsWeb
+                ? Expanded(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Expanded(child: ReviewList()),
+                      SizedBox(width: 16),
+                      Expanded(child: TeaLogList()),
+                      SizedBox(width: 16),
+                      Expanded(child: TeaCabinetList()),
+                    ],
+                  ),
+                )
+                : Expanded(
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Expanded(child: ReviewList()),
+                        SizedBox(width: 16),
+                        Expanded(child: TeaLogList()),
+                        SizedBox(width: 16),
+                        Expanded(child: TeaCabinetList()),
+                      ],
+                    ),
+                  ),
+                ),
           ],
         ),
       ),
